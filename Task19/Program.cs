15ebab2c0e4da3688ab8894cdd0ee33ace2881ca
LA <annleli@yandex.ru>
2022-09-27 20:54:40 +0300

    ДЗ с семинара от 25.09

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Clear();
+
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n < 10000 || n > 99999)
+{
+    Console.Write("Введите пятизначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+
+    int Start = n / 1000;
+    int Finish = n % 100;
+
+if (Start / 10 == Finish % 10 && Start % 10 == Finish / 10)
+    Console.Write("yes");
+else 
+    Console.Write("no");
---
+++ Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    if (i == n)
+        Console.Write(i * i * n);
+    else 
+        Console.Write(i * i * i + " | ");
+}
