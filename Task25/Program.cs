b112f943370b4f7ee84c82cdb496cf8daff7708e
LA <annleli@yandex.ru>
2022-10-04 15:55:37 +0300

    Задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введи число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("в какую степень возвести число? ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int result = 1;
+while (count <= n)
+{
+    result = result * a;
+    count++;
+}
+Console.WriteLine(result);
